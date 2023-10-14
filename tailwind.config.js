cfed98b5a3d9b30707ef13d3b58f0ffd93b46247
Aryandev12 <aryandev512@gmail.com>
2023-10-15 00:05:41 +0530

    layout done.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+      'jetbrains':[ 'JetBrains Mono']
+      },
+    },
+  },
+  plugins: [],
+}
+
